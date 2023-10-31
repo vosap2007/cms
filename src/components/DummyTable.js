590d476063703ee084f73eecc4fab1702ad3272e
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2023-10-31 12:58:31 +0200

    fix styles

--- src/components/DummyTable.js
+++ src/components/DummyTable.js
@@ -3,12 +3,6 @@ import React from 'react';
 const DummyTable = () => (
     <div>
         <h1>Dummy Table</h1>
-        {/* <table>
-            <tr>
-                <td>Dummy</td>
-                <td>Table</td>
-            </tr>
-        </table> */}
     </div>
 );
 
